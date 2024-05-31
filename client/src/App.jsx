f7de7974a70b32cd78f07aebfaf84f3d4094e42e
prapti <praptimahajan2105@gmail.com>
2024-05-31 13:28:44 +0530

    updated dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { AdminSignUp } from './views/SignUp/SignUp';
 import { EmployeeDashboard } from './views/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
 import { AdminDashboard } from './views/Dashboard/Dashboard'; // Assuming you have an AdminDashboard component
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   const [userType, setUserType] = useState(null); // Track user type ('employee' or 'admin')
@@ -42,7 +43,7 @@ const RoutesWrapper = ({ userType, setUserType }) => {
         <Route path="/forgot-password" element={<ForgotPass />} />
         <Route path="/admin-sign-up" element={<AdminSignUp />} />
         <Route path="/emp-dashboard" element={<EmployeeDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} /> {/* Add Admin Dashboard route */}
+        <Route path="/admin-dashboard" element={<Dashboard/>} /> {/* Add Admin Dashboard route */}
       </Routes>
     </>
   );
