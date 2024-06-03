2c10fd4c4f1da082283e0f9900e086b7779a9e03
Gourav Ghuge <you@example.com>
2024-06-03 22:57:11 +0530

    update the sidebar of admin and employee

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -12,7 +12,7 @@ export function EmployeeDashboard() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
 
-      <aside className=" bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4  h-[calc(90vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0" style={{ marginTop: "10rem" }}>
+      <aside className=" bg-gradient-to-br from-gray-800 to-gray-900 -translate-x-80 fixed inset-0 z-50 my-4 ml-4  h-[calc(90vh-32px)] w-72 rounded-xl transition-transform duration-300 xl:translate-x-0" style={{ marginTop: "5rem" }}>
 
 
         <div className=" relative border-b border-white/20">
