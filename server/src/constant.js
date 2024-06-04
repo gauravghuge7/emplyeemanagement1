32e8bc4160e52171d2954cbb83f43f403cc68cca
praptimahajan <143957447+praptimahajan@users.noreply.github.com>
2024-06-04 17:06:04 +0530

    Updated Constant With comments

--- server/src/constant.js
+++ server/src/constant.js
@@ -1,16 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+// Environment Variables
 export const PORT = +process.env.PORT || 8080;
 
 export const SALT_ROUND = +process.env.SALT_ROUND || 14;
 
+export const JWT_SECRET = process.env.JWT_SECRET || "somestupidsecert"
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017"
+
+// Enums
 export const USER_ROLE = Object.freeze({
     Admin: Symbol('admin'),
     User: Symbol('user'),
     Moderator: Symbol('moderator')
 })
-
-export const JWT_SECRET = process.env.JWT_SECRET || "somestupidsecert"
-
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017"
