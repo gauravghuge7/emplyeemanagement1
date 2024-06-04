03867b4b583f4951d403c7c1d108be02a5b40f07
Gourav Ghuge <you@example.com>
2024-06-04 21:29:10 +0530

    update the change

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home({loginType}) {
     <div className="flex items-center pt-20 lg:pt-36 justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-4xl p-6 bg-white rounded-lg shadow-2xl border border-gray-300">
 
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 bg-gradient-to-r from-blue-400 to-purple-400">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 bg-gradient-to-r from-blue-400 to-purple-400">
         
 
           <h1 className="text-4xl font-bold text-center mb-6">Welcome to Employee Management System</h1>
