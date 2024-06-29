9e93534da783fd2431f8a82193cba14091ae9b93
Gourav Ghuge <you@example.com>
2024-06-29 19:22:38 +0530

    update the frontend cors policy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { serverUrl } from './Url/url.backend.js'
+
 
 
 // this code is the localhost url
 import axios from 'axios'
 
-axios.defaults.baseURL = `${ serverUrl || 'http://localhost:5200'}/api/v1/`
+axios.defaults.baseURL = `https://emplyeemanagement1-production.up.railway.app/api/v1/`
 axios.defaults.withCredentials = true  // for gaingin access-control-allow-credentials
 
 
